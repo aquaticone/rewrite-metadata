c3763aa6adf4f80bc71c07c348b20a5a1d3791b0
aquatic <aquaticone@protonmail.com>
2022-07-10 17:34:20 -0600

    cleanup

--- index.cjs
+++ index.cjs
@@ -37,34 +37,9 @@ yargs(hideBin(process.argv))
         }
 
         console.log("Metadata successfully rewritten")
-      } catch (err) {}
-
-      // if (!metadataPath) {
-      //   console.error("You must provide two file paths to swap")
-      //   return
-      // }
-      // if (!pathA.includes("/") || !pathB.includes("/")) {
-      //   console.error("File paths must include a slash")
-      //   return
-      // }
-      // try {
-      //   const absPathA = path.resolve(pathA)
-      //   const absPathB = path.resolve(pathB)
-      //   const filenameA = path.basename(absPathA)
-      //   const filenameB = path.basename(absPathB)
-      //   // copy to temp directory first to make sure both files exist
-      //   const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "swap-files-"))
-      //   const tempNewA = `${tempDir}/${filenameA}`
-      //   const tempNewB = `${tempDir}/${filenameB}`
-      //   await fs.copyFile(absPathA, tempNewA)
-      //   await fs.copyFile(absPathB, tempNewB)
-      //   // copy back to original directory with names swapped
-      //   await fs.copyFile(tempNewA, absPathB)
-      //   await fs.copyFile(tempNewB, absPathA)
-      //   console.log("Filenames swapped successfully")
-      // } catch (err) {
-      //   console.error(err)
-      // }
+      } catch (err) {
+        console.error(err)
+      }
     }
   )
   .parse()
